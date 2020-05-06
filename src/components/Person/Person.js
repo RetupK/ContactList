cb8cb53e01c9e229fcb387ff9bf27382a59f3522
Krystian <retupk@gmail.com>
2020-05-06 16:09:19 +0200

    add fonts, header, and font bold for userName in modal

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -9,8 +9,11 @@ class Modal extends React.Component {
         const {removePerson, name, showModal, toggle, id} = this.props;
         let modal = showModal ? (
             <div className={`${showModal ? "modal-visible" : "modal-non-visible"}`}>
+                <div className="modal-header">
+                    <p>Remove user</p>
+                </div>
                 <div className="modal-question-container">
-                    <p>Are you sure to remove {name}?</p>
+                    <p>Are you sure to remove <span className="modal-userName">{name}</span>?</p>
                 </div>
                 <div className="modal-buttons-container">
                     <Button variant="danger" onClick={() => {removePerson(id); toggle();}}>Remove</Button>

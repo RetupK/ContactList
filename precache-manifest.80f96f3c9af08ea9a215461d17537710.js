4e6994c2f82f43cf25cbd1ec55c83d472f9544df
Krystian <retupk@gmail.com>
2020-05-06 15:04:21 +0200

    Updates

--- precache-manifest.80f96f3c9af08ea9a215461d17537710.js
+++ precache-manifest.80f96f3c9af08ea9a215461d17537710.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "/ContactList/static/js/runtime~main.d226de48.js"
   },
   {
-    "revision": "0e5d81dd966df90d9e67",
+    "revision": "9661f2cfaa54db5e8277",
     "url": "/ContactList/static/js/main.2aa5e6df.chunk.js"
   },
   {
@@ -12,15 +12,15 @@ self.__precacheManifest = [
     "url": "/ContactList/static/js/2.6ca5423e.chunk.js"
   },
   {
-    "revision": "0e5d81dd966df90d9e67",
-    "url": "/ContactList/static/css/main.f6608b67.chunk.css"
+    "revision": "9661f2cfaa54db5e8277",
+    "url": "/ContactList/static/css/main.e102eeac.chunk.css"
   },
   {
     "revision": "203b59081a1758bfb30c",
     "url": "/ContactList/static/css/2.7e6a9589.chunk.css"
   },
   {
-    "revision": "9fd13d081acfa6250fa4e06f65603784",
+    "revision": "6737864168e729d20918a43ec761fcc5",
     "url": "/ContactList/index.html"
   }
 ];
